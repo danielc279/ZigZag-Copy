96079137f6effeb59b6ad15fee39565f6ca53ca0
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-04-05 20:18:47 +0200

    Tile Generation started
    -Added tile prefabs with attachpoints
    -Function to generate a random path of 50 tiles

---
+++ ZigZag/Assets/Scripts/TileManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour 
+{
+	public GameObject[] tilePrefabs;
+
+	public GameObject currentTile;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < 50; i++)
+		{
+		SpawnTile();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void SpawnTile()
+	{
+		int randomIndex = Random.Range(0,2);
+		currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex],currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+	}
+}
