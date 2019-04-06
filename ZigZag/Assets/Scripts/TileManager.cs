a3c7e7b0549e8ef5005a310b25e4d1ee65f8e818
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-04-06 15:25:24 +0200

    Recycable Tiles
    -Made Tiles fall as you pass them and get replaced further up the level

--- ZigZag/Assets/Scripts/TileManager.cs
+++ ZigZag/Assets/Scripts/TileManager.cs
@@ -8,9 +8,41 @@ public class TileManager : MonoBehaviour
 
 	public GameObject currentTile;
 
+	private static TileManager instance;
+
+	private Stack<GameObject> leftTiles = new Stack<GameObject>();
+
+	public Stack<GameObject> LeftTiles
+	{
+		get { return leftTiles; }
+		set { leftTiles = value; }
+	}
+
+	private Stack<GameObject> topTiles = new Stack<GameObject>();
+
+	public Stack<GameObject> TopTiles
+	{
+		get { return topTiles; }
+		set { topTiles = value; }
+	}
+
+	public static TileManager Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = GameObject.FindObjectOfType<TileManager>();
+			}
+			return instance;
+		}
+	}
+
 	// Use this for initialization
 	void Start () 
-	{
+	{		
+		CreateTiles(100);
+
 		for (int i = 0; i < 50; i++)
 		{
 		SpawnTile();
@@ -23,9 +55,42 @@ public class TileManager : MonoBehaviour
 		
 	}
 
+	public void CreateTiles(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			leftTiles.Push(Instantiate(tilePrefabs[0]));
+			topTiles.Push(Instantiate(tilePrefabs[1]));
+			leftTiles.Peek().name = "LeftTile";
+			leftTiles.Peek().SetActive(false);
+			topTiles.Peek().name = "TopTile";			
+			topTiles.Peek().SetActive(false);
+		}
+	}
+
 	public void SpawnTile()
 	{
+		if (leftTiles.Count == 0 || topTiles.Count == 0)
+		{
+			CreateTiles(10);
+		}
 		int randomIndex = Random.Range(0,2);
-		currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex],currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+
+		if (randomIndex == 0)
+		{
+			GameObject tmp = leftTiles.Pop();
+			tmp.SetActive(true);
+			tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+			currentTile = tmp;
+		}
+		else if (randomIndex == 1)
+		{
+			GameObject tmp = topTiles.Pop();
+			tmp.SetActive(true);
+			tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+			currentTile = tmp;
+		}
+
+		//currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex],currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
 	}
 }
