bdc007c1b64cc0586ed8f99f73e4e3a212994dab
danielc279 <43954906+danielc279@users.noreply.github.com>
2019-04-07 18:24:57 +0200

    GameOverMenu
    -Added Canvas with GameOver
    -Displays last score and best score
    -Changes color on high score
    -Animation for entry
    -Error with changing text color

--- ZigZag/Assets/Scripts/PlayerScript.cs
+++ ZigZag/Assets/Scripts/PlayerScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -14,15 +15,21 @@ public class PlayerScript : MonoBehaviour
 
     private bool isDead;
 
+    public Animator gameOverAnim;
+
     public GameObject resetBtn;
 
-    private int score = 0;
+    public TextMeshProUGUI scoreUI;
+    public TextMeshProUGUI highscoreUI;
 
-    public Text scoreText;
+    public TextMeshProUGUI[] scoreTexts;
+    public Image background;
+    public int score { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
         isDead = false;
         dir = Vector3.zero;
     }
@@ -33,7 +40,7 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !isDead)
         {
             score++; 
-            scoreText.text = score.ToString();
+            scoreUI.SetText(score.ToString());
 
             if (dir == Vector3.forward)
             {
@@ -57,26 +64,51 @@ public class PlayerScript : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             Instantiate(ps, transform.position, Quaternion.identity);
-            score+=3; 
-            scoreText.text = score.ToString();
+            score+=2; 
+            scoreUI.SetText(score.ToString());
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        RaycastHit hit;
-
-        Ray downRay = new Ray(transform.position, -Vector3.up);
-
-        if (!Physics.Raycast(downRay, out hit))
+        if (other.tag == "Tile")
         {
-            isDead = true;
-            resetBtn.SetActive(true);
-            if (transform.childCount > 0)
-            {
-               transform.GetChild(0).transform.parent = null;
-            }
+            RaycastHit hit;
 
+            Ray downRay = new Ray(transform.position, -Vector3.up);
+
+            if (!Physics.Raycast(downRay, out hit))
+            {
+                isDead = true;
+                GameOver();
+                resetBtn.SetActive(true);
+                if (transform.childCount > 0)
+                {
+                transform.GetChild(0).transform.parent = null;
+                }
+
+            }
         }
+    }
+
+    private void GameOver()
+    {
+        gameOverAnim.SetTrigger("GameOver");
+        scoreTexts[1].SetText(score.ToString());
+
+        int bestScore = PlayerPrefs.GetInt("BestScore", 0);
+
+        if (score > bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            highscoreUI.gameObject.SetActive(true);
+            background.color = new Color32(253,68,232,255);
+            foreach (TextMeshProUGUI txt in scoreTexts)
+            {
+                txt.color = new Color32(255,255,255,255);
+            }
+        }
+
+        scoreTexts[3].SetText(PlayerPrefs.GetInt("BestScore", 0).ToString());
     }
 }
